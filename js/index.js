9c5472cc0559d9fe894d17e8cb5ff08cc0293160
CPK <samkit157@gmail.com>
2023-09-11 22:28:03 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,21 +1,40 @@
 var resource_path = 'https://raw.githubusercontent.com/nan0521/WDS-Stories-Resource/main';
 
-async function generate(){
-    const masterlist = await fetch(`${resource_path}/storylist.json`).then((res) => res.json());
-
+async function generatePage(){
+    const masterlist = await fetch(`${resource_path}/GameStoryMasterlist.json`).then((res) => res.json());
     let date = document.getElementById('latest');
-    date.innerText = `Latest Update : ${masterlist['LatestDate']}`;
+    date.innerText = `Latest Update: ${masterlist['LatestDate']}`;
 
-    const alllist = Object.values(masterlist).flat();
     let divlist = document.getElementById('stories');
     let inner = '';
-    alllist.forEach((ep)=>{
-        console.log(ep.EpisodeId)
-        if(ep.EpisodeId){
+
+    //Main Story
+    masterlist.StoryMaster.Main.forEach((main)=>{
+        main.Episode.forEach((ep)=>{
             inner += `<a href="./viewer.html?id=${ep.EpisodeId}">${ep.EpisodeId}_${ep.Title ? ep.Title : ep.EpisodeId}</a><br>`;
-        }
+        })
     })
+
+    //Event
+    masterlist.StoryMaster.Event.forEach((event)=>{
+        event.Episode.forEach((ep)=>{
+            inner += `<a href="./viewer.html?id=${ep.EpisodeId}">${ep.EpisodeId}_${ep.Title ? ep.Title : ep.EpisodeId}</a><br>`;
+        })
+    })
+
+    //Side
+    masterlist.StoryMaster.Side.forEach((side)=>{
+        side.Episode.forEach((ep)=>{
+            inner += `<a href="./viewer.html?id=${ep.EpisodeId}">${ep.EpisodeId}_${ep.Title ? ep.Title : ep.EpisodeId}</a><br>`;
+        })
+    })
+
+    //Spot
+    masterlist.StoryMaster.Spot.forEach((ep)=>{
+        inner += `<a href="./viewer.html?id=${ep.EpisodeId}">${ep.EpisodeId}_${ep.Title ? ep.Title : ep.EpisodeId}</a><br>`;
+    })
+
     divlist.innerHTML = inner;
 }
 
-generate()
+generatePage()
