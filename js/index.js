36b067783a9e3e9108d1e817d0d498dd32dfe6ab
CPK <samkit157@gmail.com>
2023-10-02 01:58:25 +0800

    poster stories

--- js/index.js
+++ js/index.js
@@ -42,13 +42,13 @@ function loadAllStories(){
 
     //Main Type
     let mainlist = document.getElementById('MainList');
-    let mainlistinner = ``
+    let mainlistinner = ``;
     masterlist.StoryMaster['Main'].forEach((group) => {
         let html = ``;
         html += `<div class="storyBlock">`;
         html += `<div class="storyIcon"><img src="./assets/logo/logo_${group.CompanyId}.png"><span>${group.CompanyId == 999 ? '序章' : `第 ${group.ChapterOrder} 章`}</span></div>`;
         html += `<div class="storyChapter">`;
-        group.Episode.forEach(ep => html += `<a href="./viewer.html?id=${ep.EpisodeId}">${ep.Title ? ep.Title : ep.EpisodeId}</a>`);
+        group.Episode.forEach(ep => html += `<a target="_blank" href="./viewer.html?id=${ep.EpisodeId}">${ep.Title ? ep.Title : ep.EpisodeId}</a>`);
         html +=  `</div></div>`;
         mainlistinner += html;
     })
@@ -56,13 +56,13 @@ function loadAllStories(){
 
     //Event Type
     let eventlist = document.getElementById('EventList');
-    let eventlistinner = ``
+    let eventlistinner = ``;
     masterlist.StoryMaster['Event'].forEach((group) => {
         let html = ``;
         html += `<div class="storyBlock">`;
         html += `<div class="storyIcon"><img src="https://raw.githubusercontent.com/nan0521/WDS-Stories-Resource/main/image/eventLogo/logo_${group.Id}.png"><span>${group.Title}</span></div>`;
         html += `<div class="storyChapter">`;
-        group.Episode.forEach(ep => html += `<a href="./viewer.html?id=${ep.EpisodeId}">${ep.Title ? ep.Title : ep.EpisodeId}</a>`);
+        group.Episode.forEach(ep => html += `<a target="_blank" href="./viewer.html?id=${ep.EpisodeId}">${ep.Title ? ep.Title : ep.EpisodeId}</a>`);
         html +=  `</div></div>`;
         eventlistinner += html;
     })
@@ -70,41 +70,55 @@ function loadAllStories(){
 
     //Side Type
     let sidelist = document.getElementById('SideList');
-    let sidelistinner = ``
+    let sidelistinner = ``;
     masterlist.StoryMaster['Side'].forEach((group) => {
-        let html = ``
+        let html = ``;
         html += `<div class="characterBlock">`;
         html += `<div class="side-header"><img src="./assets/cuteIcon/${group.Id}.png"><span>${group.Name}</span></div>`;
-        html += `<div class="side-groups">`
+        html += `<div class="side-groups">`;
         group.Groups.forEach((EPgroup)=>{
-            html += `<div class="storyBlock side-storyBlock">`
-            html += `<div class="storyIcon"><img src="https://raw.githubusercontent.com/nan0521/WDS-Stories-Resource/main/image/cardIcon/${EPgroup.Id}_0.png"></div>`
-            html += `<div class="storyChapter">`
+            html += `<div class="storyBlock side-storyBlock">`;
+            html += `<div class="storyIcon"><img src="https://raw.githubusercontent.com/nan0521/WDS-Stories-Resource/main/image/cardIcon/${EPgroup.Id}_0.png"></div>`;
+            html += `<div class="storyChapter">`;
             EPgroup.Episode.forEach(ep => {
-                html += `<a href="./viewer.html?id=${ep.EpisodeId}">${EPgroup.Title}${ep.Order == 1? '(前編)' : '(後編)'}</a>`
+                html += `<a target="_blank" href="./viewer.html?id=${ep.EpisodeId}">${EPgroup.Title}${ep.Order == 1? '(前編)' : '(後編)'}</a>`;
             })
-            html += `</div></div>`
+            html += `</div></div>`;
         })
-        html += `</div></div>`
+        html += `</div></div>`;
 
-        sidelistinner += html
+        sidelistinner += html;
     });
     sidelist.innerHTML = sidelistinner;
 
     //Spot Type
     let spotlist = document.getElementById('SpotList');
-    let spotlistinner = ``
+    let spotlistinner = ``;
     masterlist.StoryMaster['Spot'].forEach((ep) => {
-        let html = ``
-        html += `<div class="storyBlock">`
-        html += `<div class="spotIcon">`
-        ep.CharacterIds.forEach(charid => html +=  `<img src="./assets/characterlog/${charid}.png">`)
-        html += `</div>`
-        html += `<div class="storyChapter"><a href="./viewer.html?id=${ep.EpisodeId}">スポット会話ストーリー</a></div>`
-        html +=  `</div>`
-        spotlistinner += html
+        let html = ``;
+        html += `<div class="storyBlock">`;
+        html += `<div class="spotIcon">`;
+        ep.CharacterIds.forEach(charid => html +=  `<img src="./assets/characterlog/${charid}.png">`);
+        html += `</div>`;
+        html += `<div class="storyChapter"><a target="_blank" href="./viewer.html?id=${ep.EpisodeId}">スポット会話ストーリー</a></div>`;
+        html +=  `</div>`;
+        spotlistinner += html;
     })
     spotlist.innerHTML = spotlistinner;
+
+    //Poster Type
+    let posterlist = document.getElementById('PosterList');
+    posterlist.classList.add('posterlistview');
+    let posterlistinner = ``;
+    masterlist.StoryMaster['Poster'].forEach((poster) => {
+        let html = ``;
+        html += `<div class="storyBlock poster-storyBlock">`;
+        html += `<div class="storyIcon"><img src="https://raw.githubusercontent.com/nan0521/WDS-Stories-Resource/main/image/posterIcon/${poster.Id}.png"></div>`;
+        html += `<div class="storyChapter"><a target="_blank" href="./viewer.html?id=${poster.Id}">${poster.Name}</a></div>`;
+        html +=  `</div></div>`;
+        posterlistinner += html;
+    })
+    posterlist.innerHTML = posterlistinner;
 }
 
 generatePage();
--- js/viewer.js
+++ js/viewer.js
@@ -2,6 +2,7 @@ var params = new URLSearchParams(window.location.search);
 var story_id = params.get('id');
 var resource_path = 'https://raw.githubusercontent.com/nan0521/WDS-Stories-Resource/main';
 
+// default story
 var voicePlayer = new Audio()
 function playVoice(VoiceFileName){
     if (!voicePlayer.paused){
@@ -29,7 +30,7 @@ function printStoryLog(episode){
                       </div>`;
             inner += `<div class="item-voice"><img src="./assets/voice_btn.png" onclick="playVoice(${unit.VoiceFileName})"></div>`;
             inner += `<span class="item-name">${unit.SpeakerName ? unit.SpeakerName : ''}</span>`;
-            inner += `<div class="item-meg">${unit.Phrase.replace('/n', '\n')}</div>`;
+            inner += `<div class="item-meg">${unit.Phrase.replaceAll('/n', '\n')}</div>`;
             inner += `</div>`;
         }
         
@@ -38,10 +39,212 @@ function printStoryLog(episode){
     logList.innerHTML = inner;
 }
 
+//Poster story
+const charSet = [
+    {
+      "Id": 101,
+      "Name": "鳳ここな"
+    },
+    {
+      "Id": 102,
+      "Name": "静香"
+    },
+    {
+      "Id": 103,
+      "Name": "カトリナ・グリーベル"
+    },
+    {
+      "Id": 104,
+      "Name": "新妻八恵"
+    },
+    {
+      "Id": 105,
+      "Name": "柳場ぱんだ"
+    },
+    {
+      "Id": 106,
+      "Name": "流石知冴"
+    },
+    {
+      "Id": 201,
+      "Name": "連尺野初魅"
+    },
+    {
+      "Id": 202,
+      "Name": "烏森大黒"
+    },
+    {
+      "Id": 203,
+      "Name": "舎人仁花子"
+    },
+    {
+      "Id": 204,
+      "Name": "萬容"
+    },
+    {
+      "Id": 205,
+      "Name": "筆島しぐれ"
+    },
+    {
+      "Id": 301,
+      "Name": "千寿暦"
+    },
+    {
+      "Id": 302,
+      "Name": "ラモーナ・ウォルフ"
+    },
+    {
+      "Id": 303,
+      "Name": "王雪"
+    },
+    {
+      "Id": 304,
+      "Name": "リリヤ・クルトベイ"
+    },
+    {
+      "Id": 305,
+      "Name": "与那国緋花里"
+    },
+    {
+      "Id": 401,
+      "Name": "千寿いろは"
+    },
+    {
+      "Id": 402,
+      "Name": "白丸美兎"
+    },
+    {
+      "Id": 403,
+      "Name": "阿岐留カミラ"
+    },
+    {
+      "Id": 404,
+      "Name": "猫足蕾"
+    },
+    {
+      "Id": 405,
+      "Name": "本巣叶羽"
+    }
+];
+
+const eptypes = ['情報', '第一幕', '第二幕', '第三幕', '第四幕', 'アフタートーク'];
+
+function printPosterDetail(poster){
+    document.title = `${poster.Title ? poster.Title : '？？？' } | World Dai Star: Yume no Stellarium`;
+    let titlename = document.getElementById('log-title');
+    titlename.innerHTML = 'ポスターストーリー';
+
+    //select Btn
+    let logList = document.getElementById('log-list');
+    let options_left = document.getElementById('options');
+    let optioninner = ``;
+    optioninner += `<div class="aside-block poster-options">`
+    eptypes.forEach((text, index)=>{
+        optioninner += `<button value="${index}" class="poster-options-btn ${index == 0 ? 'poster-options-btn-selecting' : ''}" onclick="selectPosterContent(${index})">${text}</button>`
+
+        //content
+        let content = poster.EpisodeDetail.filter((content) => content.EpisodeType == index);
+        let element = document.createElement('div');
+        element.id = `posterEP-${index}`;
+        element.className = 'posterEPContent hidden'
+        let inner = '';
+        if(index == 5){
+            content.forEach(unit =>{
+                inner += `<div class="log-item">`;
+                inner += `<div class="item-icon">
+                            <img src="./assets/characterlog/${unit.CharacterId}.png">
+                        </div>`;
+                inner += `<span class="item-name">${charSet.find(char => char.Id == unit.CharacterId)['Name']}</span>`;
+                inner += `<div class="item-meg">${unit.Description.replaceAll('/n', '\n')}</div>`;
+                inner += `</div>`;
+            })
+        }
+        else{
+            content.forEach(unit =>{
+                inner += `<div class="poster-content">${unit.Description.replaceAll('/n', '\n')}</div>`;
+            })
+        }
+        element.innerHTML = inner;
+        logList.appendChild(element);
+    })
+    optioninner += `</div>`
+    options_left.innerHTML = optioninner
+
+
+    // let logList = document.getElementById('log-list');
+    // eptypes.forEach((_, index)=>{
+    //     let content = poster.EpisodeDetail.filter((content) => content.EpisodeType == index);
+    //     let element = document.createElement('div');
+    //     element.id = `posterEP-${index}`;
+    //     element.className = 'posterEPContent hidden'
+    //     let inner = '';
+    //     if(index == 5){
+    //         content.forEach(unit =>{
+    //             inner += `<div class="log-item">`;
+    //             inner += `<div class="item-icon">
+    //                         <img src="./assets/characterlog/${unit.CharacterId}.png">
+    //                     </div>`;
+    //             inner += `<span class="item-name">${charSet.find(char => char.Id == unit.CharacterId)['Name']}</span>`;
+    //             inner += `<div class="item-meg">${unit.Description.replaceAll('/n', '\n')}</div>`;
+    //             inner += `</div>`;
+    //         })
+    //     }
+    //     else{
+    //         content.forEach(unit =>{
+    //             inner += `<div class="poster-content">${unit.Description.replaceAll('/n', '\n')}</div>`;
+    //         })
+    //     }
+    //     element.innerHTML = inner;
+    //     logList.appendChild(element);
+    // })
+    
+    selectPosterContent(0)
+
+}
+
+function selectPosterContent(eptypes){
+
+    const btnlist = document.getElementsByClassName('poster-options-btn')
+    Array.from(btnlist).forEach(btn =>{
+        if(btn.value == eptypes){
+            btn.classList.add('poster-options-btn-selecting');
+        }else{
+            btn.classList.remove('poster-options-btn-selecting');
+        }
+    })
+
+    const contentlist = document.getElementsByClassName('posterEPContent');
+    Array.from(contentlist).forEach(con => {
+        if(con.id == `posterEP-${eptypes}`){
+            con.classList.remove('hidden');
+        }else{
+            con.classList.add('hidden');
+        }
+    })
+
+}   
+
+// 404 Page
+function show404(){
+    document.title = `404 | World Dai Star: Yume no Stellarium`;
+    let titlename = document.getElementById('log-title');
+    titlename.innerHTML = `404 NOT FOUND`;
+}
+
+// main run
 if(story_id != null){
     fetch(`${resource_path}/episode/${story_id}.json`)
         .then((res) => res.json())
+        .catch((error)=>{
+            show404();
+        })
         .then((data)=>{
-            printStoryLog(data);
-        });
+            if (data.StoryType == 5){
+                printPosterDetail(data);
+            }else{
+                printStoryLog(data);
+            }
+        })
+}else{
+    show404();
 }
